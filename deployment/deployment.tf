771438f6bbe7c23f403d35d4791d4c200c0b0567
durosyan <durosyan@outlook.com>
2020-09-22 23:22:16 +0100

    deployment change

--- deployment/deployment.tf
+++ deployment/deployment.tf
@@ -18,115 +18,105 @@ resource "aws_s3_bucket" "site" {
   }
 }
 
-variable "upload_directory" {
-  default = "public"
+locals {
+  s3_origin_id = "S3-wavexabyss.co.uk"
 }
 
-# locals {
-#   s3_origin_id = "myS3Origin"
-# }
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.site.bucket_domain_name
+    origin_id   = local.s3_origin_id
 
-# resource "aws_cloudfront_distribution" "s3_distribution" {
-#   origin {
-#     domain_name = aws_s3_bucket.b.bucket_regional_domain_name
-#     origin_id   = local.s3_origin_id
+    s3_origin_config {
+      origin_access_identity = "origin-access-identity/cloudfront/E1W2VQ3EK37X16"
+    }
+  }
 
-#     s3_origin_config {
-#       origin_access_identity = "origin-access-identity/cloudfront/ABCDEFG1234567"
-#     }
-#   }
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
 
-#   enabled             = true
-#   is_ipv6_enabled     = true
-#   comment             = "Some comment"
-#   default_root_object = "index.html"
+  aliases = ["wavexabyss.co.uk", "www.wavexabyss.co.uk"]
 
-#   logging_config {
-#     include_cookies = false
-#     bucket          = "mylogs.s3.amazonaws.com"
-#     prefix          = "myprefix"
-#   }
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
 
-#   aliases = ["mysite.example.com", "yoursite.example.com"]
+    forwarded_values {
+      query_string = false
 
-#   default_cache_behavior {
-#     allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
-#     cached_methods   = ["GET", "HEAD"]
-#     target_origin_id = local.s3_origin_id
+      cookies {
+        forward = "none"
+      }
+    }
 
-#     forwarded_values {
-#       query_string = false
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
 
-#       cookies {
-#         forward = "none"
-#       }
-#     }
+  # Cache behavior with precedence 0
+  ordered_cache_behavior {
+    path_pattern     = "/content/immutable/*"
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id = local.s3_origin_id
 
-#     viewer_protocol_policy = "allow-all"
-#     min_ttl                = 0
-#     default_ttl            = 3600
-#     max_ttl                = 86400
-#   }
+    forwarded_values {
+      query_string = false
+      headers      = ["Origin"]
 
-#   # Cache behavior with precedence 0
-#   ordered_cache_behavior {
-#     path_pattern     = "/content/immutable/*"
-#     allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-#     cached_methods   = ["GET", "HEAD", "OPTIONS"]
-#     target_origin_id = local.s3_origin_id
+      cookies {
+        forward = "none"
+      }
+    }
 
-#     forwarded_values {
-#       query_string = false
-#       headers      = ["Origin"]
+    min_ttl                = 0
+    default_ttl            = 86400
+    max_ttl                = 31536000
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
+  }
 
-#       cookies {
-#         forward = "none"
-#       }
-#     }
+  # Cache behavior with precedence 1
+  ordered_cache_behavior {
+    path_pattern     = "/*"
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
 
-#     min_ttl                = 0
-#     default_ttl            = 86400
-#     max_ttl                = 31536000
-#     compress               = true
-#     viewer_protocol_policy = "redirect-to-https"
-#   }
+    forwarded_values {
+      query_string = false
 
-#   # Cache behavior with precedence 1
-#   ordered_cache_behavior {
-#     path_pattern     = "/content/*"
-#     allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-#     cached_methods   = ["GET", "HEAD"]
-#     target_origin_id = local.s3_origin_id
+      cookies {
+        forward = "none"
+      }
+    }
 
-#     forwarded_values {
-#       query_string = false
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
+  }
 
-#       cookies {
-#         forward = "none"
-#       }
-#     }
+  price_class = "PriceClass_100"
 
-#     min_ttl                = 0
-#     default_ttl            = 3600
-#     max_ttl                = 86400
-#     compress               = true
-#     viewer_protocol_policy = "redirect-to-https"
-#   }
+  restrictions {
+    geo_restriction {
+      restriction_type = "whitelist"
+      locations        = ["US", "CA", "GB", "DE"]
+    }
+  }
 
-#   price_class = "PriceClass_200"
+  tags = {
+    project = "wavexabyss"
+  }
 
-#   restrictions {
-#     geo_restriction {
-#       restriction_type = "whitelist"
-#       locations        = ["US", "CA", "GB", "DE"]
-#     }
-#   }
-
-#   tags = {
-#     Environment = "production"
-#   }
-
-#   viewer_certificate {
-#     cloudfront_default_certificate = true
-#   }
-# }
+  viewer_certificate {
+    acm_certificate_arn = "arn:aws:acm:us-east-1:654447240886:certificate/b6c3dd1f-60e4-4739-a52a-9f8c2a58649d"
+    ssl_support_method  = "sni-only"
+  }
+}
